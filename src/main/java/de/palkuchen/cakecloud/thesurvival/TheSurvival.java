1eda85fdc2b3d082db91af5d03277b6b96355392
y0114681 <palkuchen@gmail.com>
2024-05-18 19:27:28 +0200

    Initial commit

---
+++ src/main/java/de/palkuchen/cakecloud/thesurvival/TheSurvival.java
@@ -0,0 +1,17 @@
+package de.palkuchen.cakecloud.thesurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
